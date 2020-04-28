720afeb2175c21632e513b36da69a7c1bbae4ee6
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-27 21:19:22 -0400

    Format GCinfo.java

--- src/main/java/zgc/test/GCinfo.java
+++ src/main/java/zgc/test/GCinfo.java
@@ -4,20 +4,20 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.List;
 
-public class GCinfo {
+public final class GCinfo {
 	public static final void print() {
 		try {
 			System.out.println("-".repeat(10)+" START GC INFO "+"-".repeat(10));
 			final List<GarbageCollectorMXBean> gcMxBeans = ManagementFactory.getGarbageCollectorMXBeans();
-			for (final GarbageCollectorMXBean gcMxBean : gcMxBeans) {
+			for (final var gcMxBean : gcMxBeans) {
 				System.out.println(gcMxBean.getName());
 				System.out.println(gcMxBean.getObjectName());
 			}
 			System.out.println("-".repeat(10)+" END GC INFO "+"-".repeat(10));
-		} catch (final RuntimeException re) {
-			throw re;
-		} catch (final Exception exp) {
-			throw new RuntimeException(exp);
+		} catch (final RuntimeException e) {
+			throw e;
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
 		}
    }
 }
