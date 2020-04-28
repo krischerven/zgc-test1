38426474fd4629abf60ccdf82ec00edd3d515ec7
Kris Cherven <50562493+krischerven@users.noreply.github.com>
2020-04-28 18:29:44 -0400

    Go: add missing LRU cache unit tests

--- gotest-1/src/lru_cache/fast/fast_test.go
+++ gotest-1/src/lru_cache/fast/fast_test.go
@@ -28,4 +28,16 @@ func TestLRUcache(t *testing.T) {
 		c.elements(),
 		[]int{5, 4, 1, 3},
 	)
+	assert(
+		t,
+		c.Cap() == 4,
+		c.Cap(),
+		4,
+	)
+	assert(
+		t,
+		c.Size() == 4,
+		c.Size(),
+		4,
+	)
 }
